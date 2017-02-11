f72ed908ef90e2533a09c3a1e2a7cf1092521b0a
Cristian Molina <megatux@gmail.com>
2017-02-11 16:17:27 -0300

    Add Board#to_a

--- app/models/board.rb
+++ app/models/board.rb
@@ -5,11 +5,20 @@ class Board < ApplicationRecord
   validates :num_mines, numericality: { only_integer: true, greater_than: 0 }
   validate :too_many_mines
 
+  def to_a
+    clean_board if grid.blank? || grid.size != width * height
+    grid.scan(/.{#{width}}/).map(&:chars)
+  end
+
   private
 
   def too_many_mines
     return unless num_mines && width && height
     return if num_mines < width * height
     errors.add :num_mines, 'too many mines for field size'
+  end
+
+  def clean_board
+    self.grid = ' ' * width * height
   end
 end
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
-  test "board is invalid without attributes set" do
+  test 'board is invalid without attributes set' do
     board = Board.new
     assert_not board.valid?
     assert_not board.errors[:width].empty?
@@ -9,23 +9,43 @@ class BoardTest < ActiveSupport::TestCase
     assert_not board.errors[:num_mines].empty?
   end
 
-  test "board is valid with attributes set" do
+  test 'board is valid with attributes set' do
     board = Board.new width: 2, height: 2, num_mines: 1
     assert board.valid?
   end
 
-  test "board is valid with number of mines below board cells number" do
+  test 'board is valid with number of mines below board cells number' do
     (1..5).each do |num_mines|
       board = Board.new width: 2, height: 3, num_mines: num_mines
-      byebug if !board.valid?
       assert board.valid?
     end
   end
 
-  test "board is invalid with too many mines" do
+  test 'board is invalid with too many mines' do
     [4, 5, 10, 100].each do |num_mines|
       board = Board.new width: 2, height: 2, num_mines: num_mines
       assert_not board.valid?
     end
+  end
+
+  test 'converting to an array with #to_a' do
+    # TODO: avoid duplication
+    board = Board.new width: 2, height: 2, num_mines: 2
+    assert_equal board.to_a, [[' ', ' '], [' ', ' ']]
+    board.to_a.flatten.each { |c| assert_valid_cell_value c }
+
+    board = Board.new width: 2, height: 3, num_mines: 2
+    assert_equal board.to_a, [[' ', ' '], [' ', ' '], [' ', ' ']]
+    board.to_a.flatten.each { |c| assert_valid_cell_value c }
+
+    board = Board.new width: 3, height: 2, num_mines: 2
+    assert_equal board.to_a, [[' ', ' ', ' '], [' ', ' ', ' ']]
+    board.to_a.flatten.each { |c| assert_valid_cell_value c }
+  end
+
+  private
+
+  def assert_valid_cell_value(char)
+    assert [' ', '*'].include?(char), 'not a cell value'
   end
 end
