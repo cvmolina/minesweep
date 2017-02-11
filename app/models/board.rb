3a98e0f41f7d51e8d54e1e0edfff8e3ebdc830cd
Cristian Molina <megatux@gmail.com>
2017-02-11 15:51:48 -0300

    Add number of mines validation

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,15 @@
+# Represents a Mineswipper game board
 class Board < ApplicationRecord
   validates_presence_of :width, :num_mines, :height
-  validates :width, :height, numericality: { only_integer: true, greater_than: 1}
+  validates :width, :height, numericality: { only_integer: true, greater_than: 1 }
   validates :num_mines, numericality: { only_integer: true, greater_than: 0 }
+  validate :too_many_mines
+
+  private
+
+  def too_many_mines
+    return unless num_mines && width && height
+    return if num_mines < width * height
+    errors.add :num_mines, 'too many mines for field size'
+  end
 end
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -13,4 +13,19 @@ class BoardTest < ActiveSupport::TestCase
     board = Board.new width: 2, height: 2, num_mines: 1
     assert board.valid?
   end
+
+  test "board is valid with number of mines below board cells number" do
+    (1..5).each do |num_mines|
+      board = Board.new width: 2, height: 3, num_mines: num_mines
+      byebug if !board.valid?
+      assert board.valid?
+    end
+  end
+
+  test "board is invalid with too many mines" do
+    [4, 5, 10, 100].each do |num_mines|
+      board = Board.new width: 2, height: 2, num_mines: num_mines
+      assert_not board.valid?
+    end
+  end
 end
