d82545843e610ee331c7514f10982370b2dbc972
Cristian Molina <megatux@gmail.com>
2017-02-12 21:52:25 -0300

    Respond with all users on GET /api/v1/users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   # GET /api/v1/users
   def index
-    json_response []
+    json_response User.all
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -2,7 +2,17 @@ require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   test 'respond to get index' do
+    users(:david)
+
     get api_v1_users_url
     assert_response :success
+
+    response = JSON.parse @response.body
+    assert_equal response.size, 2
+
+    assert_equal response.first['email'], 'david@here.com'
+    assert_equal response.first['name'], 'David Smith'
+    assert_equal response.last['email'], 'jane@corp.com'
+    assert_equal response.last['name'], 'Jane Steward'
   end
 end
