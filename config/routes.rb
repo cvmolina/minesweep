697c945575bdd82024a23c76099d808abb41ce30
Cristian Molina <megatux@gmail.com>
2017-02-12 21:24:30 -0300

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  concern :playable do
+    member do
+      post :step_on
+      post :flag
+      post :add_question
+      put  :freeze
+      put  :restore
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :guess, concerns: [:playable], only: [:create]
+
+      resources :users, only: [:create, :index] do
+        member do
+          resources :games, concerns: [:playable], only: [:create, :index] do
+          end
+        end
+      end
+    end
+  end
 end
